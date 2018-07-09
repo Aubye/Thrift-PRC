4ace682de75e601249242a86bcc038469df9071c
sunjc <sunjc@startimes.com.cn>
2018-07-09 19:26:53 +0800

    thrift service 注解

---
+++ src/main/java/com/platform/zookeeper/thrift/stereotype/ThriftService.java
@@ -0,0 +1,21 @@
+package com.platform.zookeeper.thrift.stereotype;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * The service Of thrift
+ */
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface ThriftService {
+
+    String name() default "";
+
+    int port() default 10000;
+
+}
